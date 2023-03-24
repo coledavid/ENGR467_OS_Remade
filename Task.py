6d51118f27ae6183538691b7da788ff5fbd60410
Sathvik <sathvik.raki@gmail.com>
2023-03-23 17:30:39 -0700

    Version 1 of EDF: Unit time periods + Updates to Task class

--- Task.py
+++ Task.py
@@ -1,7 +1,9 @@
 class Task:
-    def __init__(self, worst, actual, period_in):  # Task input constructor
+    def __init__(self, id, worst, actual, period_in):  # Task input constructor
+        self.id = id 
         self.e_wor = worst
         self.e_act = actual
         self.period = period_in # Implement comparator
         self.t_left = self.e_act # way to figure out if task has run this period
         self.has_run = False
+        self.deadline = period_in
